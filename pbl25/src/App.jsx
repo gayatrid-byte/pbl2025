665ab6a672bfd919d0205c0ec4f30f41aa90cd6a
Rashmi Abhyankar <rashmiabhyankar14@gmail.com>
2025-03-05 18:46:40 +0530

    yo

---
+++ pbl25/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+        Tailwind is Working!
+      </div>
+      
+
+
+    </>
+  )
+}
+
+export default App
